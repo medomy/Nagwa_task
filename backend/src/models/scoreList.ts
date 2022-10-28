bef64caeb22a9318cf240c89d80136612aea4b5a
Mohamed Mobarak <mohamed.mobarak@act.eg>
2022-10-28 14:08:19 +0200

    backend modified

--- backend/src/models/scoreList.ts
+++ backend/src/models/scoreList.ts
@@ -1,15 +1,24 @@
-import {promises as fsPromises} from 'fs';
-import { fileDir } from '../utils';
 import readFile from '../utils/readFile';
 
-class ScoreListStore{
-    static async knowRank(totalScore : number):Promise<number>{
-        try{
+class ScoreListStore {
+    static async knowRank(totalScore: number): Promise<number> {
+        try {
             const readedObj = await readFile();
-            const allScores = readedObj.scoresList.sort((a:number,b:number)=> a-b);
-            const rank:number = (allScores.indexOf(totalScore) / allScores.length) * 100;
-            return Number(rank.toFixed(2));
-        }catch(err){
+            const allScores: number[] = readedObj.scoresList.sort((a: number, b: number) => a - b);
+            if (allScores.includes(totalScore)) {
+                const rank: number = (allScores.indexOf(totalScore) / allScores.length) * 100;
+                return Number(rank.toFixed(2));
+            }
+            let counter = 0;
+            for(let i =0 ; i< allScores.length ; i++){
+                if(totalScore > allScores[i]){
+                    counter +=1;
+                }
+                else break;
+            }
+            console.log(counter);
+            return (counter / allScores.length) * 100;
+        } catch (err) {
             throw new Error(`can not post scores , ${err}`)
         }
     }
